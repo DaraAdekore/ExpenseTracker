14bcf0be3cf108248d4e4147a2abe6dda41e2ff3
Dara adekore <adekore8@gmail.com>
2023-05-27 16:38:42 -0700

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Dara Adekore on 2023-05-27.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
